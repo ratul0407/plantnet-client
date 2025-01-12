e84c0e80e9c967388a4e028aa74a130ef38c305f
ratul0407 <rajaulislamratul12@gmail.com>
2025-01-12 19:10:45 +0600

    part 5 27min

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,13 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
 
 const AddPlant = () => {
+  //handle form submit
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+  };
   return (
     <div>
       <Helmet>
@@ -9,9 +15,9 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleSubmit={handleSubmit} />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
