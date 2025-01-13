afb81dbb90329b735a2e723eb1488e173d0b791a
ratul0407 <rajaulislamratul12@gmail.com>
2025-01-13 21:42:58 +0600

    day4 6:38

--- src/components/Modal/PurchaseModal.jsx
+++ src/components/Modal/PurchaseModal.jsx
@@ -5,68 +5,170 @@ import {
   TransitionChild,
   DialogPanel,
   DialogTitle,
-} from '@headlessui/react'
-import { Fragment } from 'react'
-
-const PurchaseModal = ({ closeModal, isOpen }) => {
+} from "@headlessui/react";
+import { Fragment, useState } from "react";
+import Button from "../../components/Shared/Button/Button";
+import useAuth from "../../hooks/useAuth";
+import toast from "react-hot-toast";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+const PurchaseModal = ({ closeModal, isOpen, plant, refetch }) => {
+  const axiosSecure = useAxiosSecure();
   // Total Price Calculation
+  const { user } = useAuth();
 
+  const { _id, name, quantity, category, seller, price } = plant;
+  const [totalQuantity, setTotalQuantity] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(price);
+  const [purchaseInfo, setPurchaseInfo] = useState({
+    customer: {
+      name: user?.displayName,
+      email: user?.email,
+      image: user?.photoURL,
+    },
+    plantId: _id,
+    seller: seller?.email,
+    address: "",
+    status: "Pending",
+  });
+  console.log(totalQuantity);
+  const handleQuantity = (value) => {
+    if (value > quantity) {
+      value = 20;
+      setTotalQuantity(quantity);
+      setTotalPrice(quantity * price);
+      return toast.error("Quantity Exceeds available stock", {
+        duration: 2000,
+      });
+    }
+    if (value < 0) {
+      setTotalQuantity(1);
+      return toast.error("Quantity cannot be less than 1");
+    }
+    setTotalQuantity(value);
+    setTotalPrice(value * price);
+    console.log(`Value ${value}`, `total Quantity ${totalQuantity}`);
+    setPurchaseInfo((prev) => {
+      return { ...prev, quantity: value, price: value * price };
+    });
+  };
+
+  const handlePurchase = async () => {
+    console.log(totalPrice, totalQuantity);
+    console.table(purchaseInfo);
+    try {
+      const data = await axiosSecure.post("/orders", purchaseInfo);
+      console.log(data);
+
+      await axiosSecure.patch(`/plants/quantity/${_id}`, {
+        quantityToUpdate: totalQuantity,
+      });
+      toast.success("Order SuccessFull!");
+      refetch();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      closeModal();
+    }
+  };
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <TransitionChild
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </TransitionChild>
 
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <TransitionChild
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+              <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <DialogTitle
-                  as='h3'
-                  className='text-lg font-medium text-center leading-6 text-gray-900'
+                  as="h3"
+                  className="text-lg font-medium text-center leading-6 text-gray-900"
                 >
                   Review Info Before Purchase
                 </DialogTitle>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Plant: Money Plant</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Plant: {name}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Category: Indoor</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Category: {category}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Customer: PH</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Customer: {user?.displayName}
+                  </p>
                 </div>
 
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Price: $ 120</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Price: ${price}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Available Quantity: 5</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Available Quantity: {quantity}
+                  </p>
+
+                  {/* quantity input field */}
+                  <div className="text-sm flex items-center gap-6">
+                    <label htmlFor="quantity" className="block text-gray-600">
+                      Quantity:
+                    </label>
+                    <input
+                      onChange={(e) => handleQuantity(parseInt(e.target.value))}
+                      value={totalQuantity}
+                      className="w-full py-3 pl-4 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                      name="quantity"
+                      id="quantity"
+                      type="number"
+                      placeholder="Available quantity"
+                      required
+                    />
+                  </div>
                 </div>
+                <div className="mt-3"></div>
+                {/* address input field */}
+                <div className=" text-sm flex items-center gap-6 pb-4">
+                  <label htmlFor="address" className="block text-gray-600">
+                    Address:
+                  </label>
+                  <input
+                    onChange={(e) =>
+                      setPurchaseInfo((prev) => {
+                        return { ...prev, address: e.target.value };
+                      })
+                    }
+                    className="w-full px-4 py-3 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                    name="quantity"
+                    id="address"
+                    type="text"
+                    placeholder="Address"
+                    required
+                  />
+                </div>
+                <p>Total Price: ${totalPrice}</p>
+                <Button onClick={handlePurchase} label={`pay $${totalPrice}`} />
               </DialogPanel>
             </TransitionChild>
           </div>
         </div>
       </Dialog>
     </Transition>
-  )
-}
+  );
+};
 
-export default PurchaseModal
+export default PurchaseModal;
--- src/pages/Dashboard/Customer/MyOrders.jsx
+++ src/pages/Dashboard/Customer/MyOrders.jsx
@@ -1,66 +1,69 @@
-import { Helmet } from 'react-helmet-async'
-import CustomerOrderDataRow from '../../../components/Dashboard/TableRows/CustomerOrderDataRow'
+import { Helmet } from "react-helmet-async";
+import CustomerOrderDataRow from "../../../components/Dashboard/TableRows/CustomerOrderDataRow";
+import useAuth from "../../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const MyOrders = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: orders,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["orders", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/customer-orders/${user?.email}`);
+      return data;
+    },
+  });
+
+  console.log(orders);
   return (
     <>
       <Helmet>
         <title>My Orders</title>
       </Helmet>
-      <div className='container mx-auto px-4 sm:px-8'>
-        <div className='py-8'>
-          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
-            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
-              <table className='min-w-full leading-normal'>
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Image
                     </th>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Name
                     </th>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Category
                     </th>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Price
                     </th>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Quantity
                     </th>
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Status
                     </th>
 
-                    <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
-                    >
+                    <th scope="col" className="table-head">
                       Action
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <CustomerOrderDataRow />
+                  {orders?.map((order) => {
+                    console.log(order);
+                    return (
+                      <CustomerOrderDataRow key={order._id} order={order} />
+                    );
+                  })}
                 </tbody>
               </table>
             </div>
@@ -68,7 +71,7 @@ const MyOrders = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default MyOrders
+export default MyOrders;
